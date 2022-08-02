6e1802735c26ff7adbf768bd3e9ed5409f895876
Moody Amin <moodyamin531@gmail.com>
2022-08-02 17:53:47 +0300

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
         <div className="bg-stone-700 flex justify-center items-center h-[8vh] absolute inset-x-0 bottom-0">
-            <p className="text-center opacity-70 text-slate-50 font-semibold">Copyright 2022 Moody's Next JS project</p>
+            <p className="text-center opacity-70 text-slate-50 font-semibold">Copyright 2022 Moody &apos; s Next JS project</p>
         </div>
     );
 }
