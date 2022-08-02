856bf3a42e931cc984b2ce28a83657d82e878d83
Moody Amin <moodyamin531@gmail.com>
2022-08-02 17:51:31 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
             </Head>
             <div className="maindiv">
                 <div className="innermain">
-                    <h2 className="titles">Moody's Next JS Learning</h2>
+                    <h2 className="titles">Moody&apos;s Next JS Learning</h2>
                     <p className="paragraphs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus enim similique in ipsum quisquam cupiditate labore velit, quos quasi quaerat, ratione nostrum aut eaque, repellendus rerum doloremque quo excepturi perspiciatis veritatis impedit. Quasi officia repellendus, nostrum deserunt voluptatem, facere eaque sed illo at cum deleniti.</p>
                     <p className="paragraphs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ad, sed expedita labore unde eum? Repellat, voluptatibus, enim iste totam ab sunt ad suscipit aliquid distinctio quos amet. Rerum, quos error natus nisi veritatis nostrum aperiam commodi dolorum, dignissimos aut harum ex tenetur. Nostrum, unde?</p>
                 </div>
